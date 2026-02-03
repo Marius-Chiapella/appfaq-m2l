895083986796d87b4db94d6d6dd607565a115151
Marius Chiapella <marius.chiapella@limayrac.fr>
2026-02-03 16:30:43 +0100

    reorganisation

---
+++ appfaq-site/pages/subpages/list_subpages/delete.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>M2L-delete</title>
+</head>
+<body>
+  <h1>M2L-delete</h1>
+<h3>Question</h3>
+<textarea disabled placeholder = "Question" rows="4" cols="50">
+</textarea><br>
+<h3>Réponce</h3>
+<textarea disabled placeholder = "Réponse à la question" rows="4" cols="50">
+</textarea><br>
+<button type="submit">Supprimer</button>
+<button type="submit">Annuler</button>
+<br><br>
+
+<a href="../list.php">Page list</a> <br>
+</body>
+</html>
