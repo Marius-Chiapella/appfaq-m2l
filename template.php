d28667dfb0c4a21dea8c0b2a25e1a38770cab02a
Marius Chiapella <marius.chiapella@limayrac.fr>
2026-02-03 15:05:03 +0100

    template site

---
+++ template.php
@@ -0,0 +1,33 @@
+<?php
+$titre = "Template";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="appfaq-site/css/main.css">
+    <title>ProjetM2L</title>
+</head>
+
+<body>
+    <div class="barre_haute">
+        <h2>AppFaq - M2L</h2>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+    <div class="content">
+        <h1><?= $titre ?></h1>
+
+    </div>
+
+    <div class="footer">
+        <p>placeholder</p>
+    </div>
+
+</body>
+
+</html>
