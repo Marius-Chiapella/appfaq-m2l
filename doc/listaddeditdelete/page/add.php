78a4d9f8c8636396f941875b931cbc47c0bbe9e5
liamlcn <liam.lacan@limayrac.fr>
2026-02-03 15:19:43 +0100

    Add files via upload

---
+++ doc/listaddeditdelete/page/add.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>M2L-add</title>
+</head>
+<body>
+  <h1>M2L-add</h1>
+<h3>Question</h3>
+<textarea placeholder = "Ecrire un commentaire" rows="4" cols="50">
+</textarea><br>
+<button type="submit">Enregister</button>
+<button type="submit">Annuler</button>
+<br><br>
+
+<a href="../list.php">Page list</a> <br>
+</body>
+</html>
