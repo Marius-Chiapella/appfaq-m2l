873053ec45994455eb450d7b4ffc5d972522da1b
Marius Chiapella <marius.chiapella@limayrac.fr>
2026-02-10 15:28:36 +0100

    list base made

--- appfaq-site/pages/subpages/list.php
+++ appfaq-site/pages/subpages/list.php
@@ -1,15 +1,33 @@
 <?php
-$placeholder = array(
-    "placeholder1",
-    "placeholder2",
-    "placeholder3",
-    "placeholder4",
-    "placeholder5",
-);
+include "fonctions.inc.php";
+session_start();
+
+// Redirige vers la page de connexion si on n'est pas connecté
+if (isset($_SESSION['username'])) {
+    header("Location: ../index.php");
+    exit();
+} else {
+    // $username = $_SESSION['username'];
+    $username = "jef";
+}
+
+// Connexion à la base de données
+$dbh = connexion();
+
+// Liste des utilisateurs
+$sql = "select * from v_faq
+        WHERE pseudo = 'jef';";
+try {
+    $sth = $dbh->prepare($sql);
+    $sth->execute();
+    $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $ex) {
+    die("Erreur lors de la requête SQL : ".$ex->getMessage());
+}
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,23 +36,20 @@ $placeholder = array(
 </head>
 <body>
   <h1>M2L-list</h1>
+  <p>User : <?= $username ?></p>
   <table>
-  <tr>
-    <th>NR</th>
-    <th>Auteur</th>
-    <th>Question</th>
-    <th>Réponse</th>
-    <th>Action</th>
-  </tr>
-  <?php foreach ($placeholder as $value ) { ?>
     <tr>
-      <td><?= $value ?></td>
-      <td><?= $value ?></td>
-      <td><?= $value ?></td>
-      <td><?= $value ?> </td>
-      <td><?= $value ?></td>
+      <th>NR</th>
+      <th>Auteur</th>
+      <th>Question</th>
+      <th>Réponse</th>
     </tr>
-<?php } ?>
+    <?php
+foreach ($rows as $row) {
+    echo "<tr><td>".$row['id_user']."</td><td>".$row['pseudo']."</td><td>".$row['question']."</td><td>". $row['reponse'] ."</td></tr>";
+}
+?>
+  </table>
 </table>
 <a href="page/add.php">Page add</a> <br>
 <a href="page/edit.php">Page edit</a> <br>
