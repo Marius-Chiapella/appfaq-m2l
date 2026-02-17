16503d72094d526dd8c4d5ddfbac27d9779db4e9
Marius Chiapella <marius.chiapella@limayrac.fr>
2026-02-17 16:20:13 +0100

    add file

--- appfaq-site/pages/subpages/list_subpages/add.php
+++ appfaq-site/pages/subpages/list_subpages/add.php
@@ -1,23 +1,37 @@
 <?php
-
+$titre = "Ajouter question";
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
-  <title>M2L-add</title>
-</head>
-<body>
-  <h1>M2L-add</h1>
-<h3>Question</h3>
-<textarea placeholder = "Ecrire un commentaire" rows="4" cols="50">
-</textarea><br>
-<button type="submit">Enregister</button>
-<button type="submit">Annuler</button>
-<br><br>
+<html lang="fr">
 
-<a href="../list.php">Page list</a> <br>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>ProjetM2L</title>
+</head>
+
+<body>
+    <div class="barre_haute">
+        <h2>AppFaq - M2L</h2>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </div>
+
+    <div class="content">
+        <h1><?= $titre ?></h1>
+        <a href="../../index.php">Accueil</a> <br> <br> <br>
+
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+            <textarea name="question" id="question" cols="100" rows="20" style="resize: none;"></textarea>
+        </form>
+    </div>
+
+    <div class="footer">
+        <p>placeholder</p>
+    </div>
+
 </body>
+
 </html>
