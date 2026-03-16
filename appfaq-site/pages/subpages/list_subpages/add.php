338d8f2e475fabb3edeb0d91f275bd9c09dc02a3
Marius Chiapella <marius.chiapella@limayrac.fr>
2026-03-16 08:34:50 +0100

    add page

--- appfaq-site/pages/subpages/list_subpages/add.php
+++ appfaq-site/pages/subpages/list_subpages/add.php
@@ -1,5 +1,51 @@
 <?php
-$titre = "Ajouter question";
+session_start();
+require_once '../fonctions.inc.php';
+$titre = "Ajouter page";
+$pdo = connexion();
+$submit = isset($_POST['submit']);
+
+$_SESSION['id_user'] = 1;
+$_SESSION['id_usertype'] = 3;
+$username = "jef";
+
+if (($_SESSION['id_user']) == '') {
+    header('Location: ../../index.php');
+    exit;
+}
+
+$id_usertype = $_SESSION['id_usertype'] ?? null;
+if ($id_usertype == null) {
+    die("Accès refusé : droits insuffisants.");
+}
+if ($submit) {
+
+    if (isset($_POST['annuler'])) {
+        header('Location: ../list.php');
+        exit;
+    }
+
+    $question = $_POST['question'] ?? '';
+
+    if ($question == '') {
+        $error = "La question est obligatoire.";
+    } else {
+        $now = date('Y-m-d H:i:s');
+
+        $sql = "INSERT INTO faq (question, dat_question, id_user) 
+                      VALUES (:question, :dat_question, :id_user)";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            ':question' => $question,
+            ':dat_question' => $now,
+            ':id_user' => $_SESSION['id_user'],
+        ]);
+
+        header('Location: ../list.php');
+        exit;
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,8 +69,15 @@ $titre = "Ajouter question";
         <h1><?= $titre ?></h1>
         <a href="../../index.php">Accueil</a> <br> <br> <br>
 
+        <?php if (isset($error)):
+           echo "<p> $error </p>";
+        endif; ?>
+
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <textarea name="question" id="question" cols="100" rows="20" style="resize: none;"></textarea>
+            <br><br>
+            <input type="submit" name='submit' value="Ajouter">
+            <input type="button" name='annuler' value="Annuler">
         </form>
     </div>
 
